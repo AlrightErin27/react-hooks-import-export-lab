1956f9199c8eadc6eee8ce7453ebfb569e255c52
Erin Van Brunt <erinmontybruce@gmail.com>
2022-03-16 14:36:19 -0700

    done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { username, city, image } from "../data/user";
 
 function Home() {
   return (
@@ -6,6 +7,8 @@ function Home() {
       <h1>
         {username} is a Web Developer from {city}
       </h1>
+      {image}
     </div>
   );
 }
+export default Home;
